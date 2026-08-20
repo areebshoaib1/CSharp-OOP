9b37371a2774fb8ff2514909982dc8fbb134201d
areebshoaib1 <areebspecial1@gmai.com>
2026-08-21 04:12:39 +0500

    Solve: Define IPayable with method ProcessPayment(decimal amount). Implement it in CreditCard and PayPal classes with different logic.

---
+++ ConsoleApp1/Payment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace OOP_Practice
+{
+    internal interface IPayable
+    {
+        public decimal ProcessPayment(decimal amount);
+    }
+    internal class Paypal : IPayable
+    {
+        private const decimal PayPal_tax = 0.05m;
+        private decimal Tax_calculator;
+        private decimal Sentable_amount;
+        public decimal ProcessPayment(decimal amount)
+        {
+            Tax_calculator = amount * PayPal_tax;
+            Sentable_amount = amount - Tax_calculator;
+            Console.WriteLine($"Processed {amount} through Paypal\n{Sentable_amount} will receive by receiver");
+            return Sentable_amount;
+        }
+    }
+    internal class CreditCard : IPayable
+    {
+        private const decimal CreditCard_tax = 0.1m;
+        private decimal Tax_calculator;
+        private decimal Sentable_amount;
+        public decimal ProcessPayment(decimal amount)
+        {
+            Tax_calculator = amount * CreditCard_tax;
+            Sentable_amount = amount - Tax_calculator;
+            Console.WriteLine($"\nProcessed {amount} through Credit Card\n{Sentable_amount} will receive by receiver");
+            return Sentable_amount;
+        }
+    }
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ConsoleApp1
+﻿
+using System;
+namespace OOP_Practice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Define IPayable with method ProcessPayment(decimal amount). Implement it in CreditCard and PayPal classes with different logic.*/
+
+            Paypal P1 = new Paypal();
+            P1.ProcessPayment(10000);
+
+            CreditCard C1 = new CreditCard();
+            C1.ProcessPayment(10000);
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
