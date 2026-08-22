905043269560e4951f0f3a40978c458c74345375
areebshoaib1 <areebspecial1@gmai.com>
2026-08-22 05:15:36 +0500

    Solve: Create BankAccount with deposit and withdrawal and check balance

---
+++ 01_Classes_And_Objects/02_BankAccountSystem/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_BankAccountSystem
+{
+    internal class BankAccount
+    {
+
+        private decimal _balance = 0m;
+
+        internal void Deposit(decimal amount)
+        {
+            _balance += amount;
+            Console.WriteLine($"Deposited {amount}  Successfully");
+        }
+        internal void Withdraw(decimal amount)
+        {
+            if (amount <= _balance)
+            {
+
+                _balance -= amount;
+                Console.WriteLine($" {amount}  Withdrawn Successfully");
+            }
+            else
+
+                Console.WriteLine("Withdraw Balance is higher then Balance avaialable in your Account");
+        }
+        
+        internal void CheckBalance()
+        {
+
+            Console.WriteLine($"Balance available: {_balance}");
+        }
+
+
+
+    }
+}
--- 01_Classes_And_Objects/02_BankAccountSystem/Program.cs
+++ 01_Classes_And_Objects/02_BankAccountSystem/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            BankAccount B1 = new BankAccount();
+
+            B1.CheckBalance();
+            B1.Deposit(500);
+            B1.Withdraw(250);
+            B1.CheckBalance();
         }
     }
 }
