88607ee62df561d13e400df7f93d6d528056338a
areebshoaib1 <areebspecial1@gmai.com>
2026-08-22 22:07:30 +0500

    Solve: Create a Rectangle class with Calculate Area and Calculate Perimeter

---
+++ 01_Classes_And_Objects/03_RectangleAreaPerimeter/AreaPerimeter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_RectangleAreaPerimeter
+{
+    internal class Rectangle
+    {
+        private double Length;
+        private double Width;
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+
+        }
+
+        public void CalculateArea()
+        {
+            Console.WriteLine($"Area = {Length * Width}");
+        }
+        public void CalculatePerimeter()
+        {
+            Console.WriteLine($"Perimeter = {2 * (Length + Width)}");
+        }
+
+    }
+}
---
+++ 01_Classes_And_Objects/03_RectangleAreaPerimeter/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _03_RectangleAreaPerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle R1 = new Rectangle(3.5, 6.1);
+            R1.CalculateArea();
+            R1.CalculatePerimeter();
+
+        }
+    }
+}
