2695c63289337d23b0884854da4962cadd6d617c
areebshoaib1 <areebspecial1@gmai.com>
2026-08-23 03:44:55 +0500

    Solve: Car class with start, stop, and display info

---
+++ 01_Classes_And_Objects/04_CarStartStop/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04_CarStartStop
+{
+    internal class Car
+    {
+        private readonly string _brandName;
+        private readonly int _topSpeed;
+        public Car(string brandName, int topSpeed)
+        {
+            _brandName = brandName;
+            _topSpeed = topSpeed;
+
+
+        }
+        public void Start()
+        {
+            Console.WriteLine($"{_brandName} Started!");
+        }
+        public void Stop()
+        {
+            Console.WriteLine($"{_brandName} Stopped!");
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Details:\n Brand Name: {_brandName}\n Top Speed: {_topSpeed}km/h");
+        }
+    }
+
+
+}
+
---
+++ 01_Classes_And_Objects/04_CarStartStop/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04_CarStartStop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car C1 = new Car(" Mercedes-Benz", 300);
+            Car C2 = new Car(" Lamborghini cian", 350);
+
+            C1.Start();
+            C1.Stop();
+            C1.DisplayInfo();
+            Console.WriteLine();
+            C2.Start();
+            C2.Stop();
+            C2.DisplayInfo();
+
+
+        }
+    }
+}
