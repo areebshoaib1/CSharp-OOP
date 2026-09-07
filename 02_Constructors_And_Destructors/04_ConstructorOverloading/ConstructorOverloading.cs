5f05f0a072bde13f0baeb2e41f8496667a7cc733
areebshoaib1 <areebspecial1@gmai.com>
2026-09-07 22:21:50 +0500

    Solve : Perform and Demonstrate Constructor Overloading

---
+++ 02_Constructors_And_Destructors/04_ConstructorOverloading/ConstructorOverloading.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace _04_ConstructorOverloading
+{
+    internal class ConstructorOverloading
+    {
+        private readonly string name;
+        private readonly string id;
+        private readonly decimal salary;
+
+        public ConstructorOverloading()
+        {
+            name = " ";
+            id = " ";
+            salary = 0m;
+            Console.WriteLine($" Default Constructor called!");
+
+        }
+        public ConstructorOverloading(string ID)
+        {
+            id = ID;
+            name = " ";
+            Console.WriteLine($" Constructor with one parameter called!");
+
+        }
+        public ConstructorOverloading(string ID, string Name, int Salary)
+        {
+            id = ID;
+            name = Name;
+            salary = Salary;
+            Console.WriteLine($" Constructor with three parameter called!");
+
+        }
+        public ConstructorOverloading(string Name, int Salary, string ID)
+        {
+            id = ID;
+            name = Name;
+            salary = Salary;
+            Console.WriteLine($" Constructor with three parameter  but different sequence called!");
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ 02_Constructors_And_Destructors/04_ConstructorOverloading/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04_ConstructorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConstructorOverloading CO1 = new ConstructorOverloading();
+
+            ConstructorOverloading CO2 = new ConstructorOverloading("201");
+
+            ConstructorOverloading CO3 = new ConstructorOverloading("201", "ABC", 45000);
+
+            ConstructorOverloading CO4 = new ConstructorOverloading("201", 45000, "ABC");
+        }
+    }
+}
