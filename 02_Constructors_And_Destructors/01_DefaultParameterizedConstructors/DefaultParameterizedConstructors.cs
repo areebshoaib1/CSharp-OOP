d4333d489acdbd5292093fc84d34ca61f5a0b839
areebshoaib1 <areebspecial1@gmai.com>
2026-09-07 18:35:20 +0500

    Solve: Create a Default Constructor and Parameterized Constructor

---
+++ 02_Constructors_And_Destructors/01_DefaultParameterizedConstructors/DefaultParameterizedConstructors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_DefaultParameterizedConstructors
+{
+    internal class Product
+    {
+        private readonly string Name;
+        private readonly decimal Price;
+
+        public Product()
+        {
+            Name = "Unknown";
+            Price = 0;
+            Console.WriteLine($"Default Constructor:\n Name: {Name}\n Price: {Price}");
+        }
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+            Console.WriteLine($"Parameterized Constructor:\n Name: {Name}\n Price: {Price}");
+        }
+
+    }
+}
---
+++ 02_Constructors_And_Destructors/01_DefaultParameterizedConstructors/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _06_DefaultParameterizedConstructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product P1 = new Product();
+            Product P2 = new Product("Snacks", 10);
+
+        }
+    }
+}
