78260d98cc9c2e2cf3e24fc7271c4e8233c43e70
areebshoaib1 <areebspecial1@gmai.com>
2026-09-07 22:25:04 +0500

    Solve: Constructor and Destructor call order garbage Collector()

---
+++ 02_Constructors_And_Destructors/05_ConstructorDestructorOrder/ConstructorDestructorOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05_ConstructorDestructorOrder
+{
+    internal class Parent
+
+    {
+        public Parent()
+        {
+            Console.WriteLine("Parent Constructor called!");
+        }
+        ~Parent()
+        {
+            Console.WriteLine("Parent Destructor called!");
+        }
+    }
+
+    internal class Child : Parent
+    {
+        public Child()
+        {
+            Console.WriteLine("Child Constructor called!");
+        }
+
+        ~Child()
+        {
+            Console.WriteLine("Child Destructor called!");
+        }
+
+
+    }
+}
---
+++ 02_Constructors_And_Destructors/05_ConstructorDestructorOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05_ConstructorDestructorOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateObject();
+
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.ReadLine();
+        }
+
+
+        static void CreateObject()
+        {
+            Child C1 = new Child();
+        }
+    }
+}
