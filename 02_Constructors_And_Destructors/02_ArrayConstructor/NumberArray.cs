8e0c009d0bb33cedae692b5ca07acee0dfa4acca
areebshoaib1 <areebspecial1@gmai.com>
2026-09-07 21:04:13 +0500

    Solve:Iniatialize an array using constructor recv size as parameter

---
+++ 02_Constructors_And_Destructors/02_ArrayConstructor/NumberArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_ArrayConstructor
+{
+    internal class NumberArray
+    {
+        private int[] arr;
+        private int Length;
+        public NumberArray(int length)
+        {
+            Length = length;
+
+            arr = new int[length];
+            for (int i = 1; i <= length; i++)
+            {
+                arr[i-1] = i;
+            }
+
+        }
+
+        public void Display()
+        {
+
+            Console.Write("[");
+            for (int i = 0; i < Length; i++)
+            {
+                Console.Write($" {arr[i]} ");
+            }
+            Console.Write("]");
+
+        }
+
+
+    }
+}
---
+++ 02_Constructors_And_Destructors/02_ArrayConstructor/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _02_ArrayConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberArray N1 = new NumberArray(20);
+            N1.Display();
+        }
+    }
+}
