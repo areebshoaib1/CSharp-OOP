2f84049dacfbf5d25b472b038bbc3179ba39baff
areebshoaib1 <areebspecial1@gmai.com>
2026-09-07 21:09:49 +0500

    Solve : Count how many objects created from the class using Constructor

---
+++ 02_Constructors_And_Destructors/03_ConstructorObjectCounter/ObjectCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_ConstructorObjectCounter
+{
+    internal class Student
+    {
+        private static int objectcount = 0;
+
+        public Student()
+        {
+            objectcount++;
+        }
+
+        public int GetCount()
+        {
+            return objectcount;
+        }
+    }
+}
---
+++ 02_Constructors_And_Destructors/03_ConstructorObjectCounter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03_ConstructorObjectCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S1 = new Student();
+            Console.WriteLine($"{S1.GetCount()}");
+
+            Student S2 = new Student();
+            Console.WriteLine($"{S2.GetCount()}");
+
+            Student S3 = new Student();
+            Console.WriteLine($"{S3.GetCount()}");
+
+
+
+            //S2.GetCount();
+            //S3.GetCount();
+
+        }
+    }
+}
